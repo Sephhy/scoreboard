bf35575cc2f034a4b587788fd96424c2046e49d2
Seph <167627426+Sephhy@users.noreply.github.com>
2024-06-18 09:15:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+let homeTeamScore = document.getElementById("home-team-score")
+
+let awayTeamScore = document.getElementById("away-team-score")
+
+let quarterOne = document.getElementById("quarter-one")
+
+let quarterTwo = document.getElementById("quarter-two")
+
+let quarterThree = document.getElementById("quarter-three")
+
+let quarterFour = document.getElementById("quarter-four")
+
+let quarter = document.getElementById("quarter")
+
+
+
+let homeScore = 0
+let awayScore = 0
+
+// 
+
+function homePlusOne() {
+    homeScore += 1
+    homeTeamScore.textContent = homeScore
+}
+
+function homePlusTwo() {
+    homeScore += 2
+    homeTeamScore.textContent = homeScore
+}
+
+function homePlusThree() {
+    homeScore += 3
+    homeTeamScore.textContent = homeScore
+}
+
+function awayPlusOne() {
+    awayScore += 1
+    awayTeamScore.textContent = awayScore
+}
+
+function awayPlusTwo() {
+    awayScore += 2
+    awayTeamScore.textContent = awayScore
+}
+
+function  awayPlusThree() {
+    awayScore += 3
+    awayTeamScore.textContent = awayScore
+}
+
+function newGame() {
+    homeScore = 0
+    homeTeamScore.textContent = homeScore
+    awayScore = 0
+    awayTeamScore.textContent = awayScore
+}
+
+
+
+
+
